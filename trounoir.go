bfb7c4acb5eb8ff74964fcea8e2f55b0edb5ff5d
Kelvin Wang <sherkrainwang@gmail.com>
2015-11-12 14:25:08 -0800

    add buffer with bug

--- trounoir.go
+++ trounoir.go
@@ -3,63 +3,74 @@ package trounoir
 import (
 	"container/list"
 	"github.com/boltdb/bolt"
+	"strings"
 	"sync"
 )
 
+const (
+	MAX_MEM_BUFFER_KEY = 2000
+)
+
+// collection of db
 type trounoir struct {
-	WriterBuffer *ringBuffer
-	ReaderBuffer *ringBuffer
-	Bolts        []*bolt.DB
-	MemBuffer    []*bucketBuffer
+	InputChannel    chan *item
+	OutputChannel   chan *item
+	Bolts           []*bolt.DB
+	MemBuffer       map[string]*bucketBuffer
+	NumMemBufferKey int
+	keyQueque       *list.List
 }
 
-func New(bolt_cluster []string, db_name string) {
-
+func New(bolt_cluster []string, db_name string) *trounoir {
+	tr := &trounoir{}
+	tr.InputChannel = make(chan *item)
+	tr.OutputChannel = make(chan *item, len(bolt_cluster))
+	return tr
 }
 
 type bucketBuffer struct {
 	sync.Mutex
-	data      map[string][]byte
-	keyQueque *list.List
+	data map[string][]byte
 }
 
 func (bb *bucketBuffer) remove(key string) {
 	bb.Lock()
 	delete(bb.data, key)
 	bb.Unlock()
-	go func(k string) {
-		var next *list.Element
-		for e := bb.keyQueque.Front(); e != nil; e = next {
-			next = e.Next()
-			if k == e.Value.(string) {
-				bb.keyQueque.Remove(e)
-			}
-		}
-	}(key)
 }
 
-func (bb *bucketBuffer) add(key string, b []byte) {
+// add a key to bucket
+func (tr *trounoir) add(bucket string, key string, b []byte) {
+	bb := tr.MemBuffer[bucket]
 	bb.Lock()
-
+	tr.InputChannel <- &item{bucket, key, b}
+	bb.data[key] = b
+	tr.keyQueque.PushFront(bucket + " " + key)
+	if tr.NumMemBufferKey > MAX_MEM_BUFFER_KEY {
+		last_bucket_key := tr.keyQueque.Back()
+		i := strings.Index(last_bucket_key.Value.(string), " ")
+		b2 := last_bucket_key.Value.(string)[:i]
+		k2 := last_bucket_key.Value.(string)[i+1:]
+		tr.keyQueque.Remove(last_bucket_key)
+		delete(tr.MemBuffer[b2].data, k2)
+	} else {
+		tr.NumMemBufferKey++
+	}
 	bb.Unlock()
 }
 
-type ringBuffer struct {
-	inputChannel  <-chan int
-	outputChannel chan int
+type item struct {
+	bucket string
+	key    string
+	b      []byte
 }
 
-func newRingBuffer(inputChannel <-chan int, outputChannel chan int) *ringBuffer {
-	return &ringBuffer{inputChannel, outputChannel}
-}
-
-func (r *ringBuffer) run() {
-	for v := range r.inputChannel {
-		select {
-		case r.outputChannel <- v:
-		default:
-			<-r.outputChannel
-			r.outputChannel <- v
-		}
+func (tr *trounoir) process() {
+	for i := range tr.Bolts {
+		out := <-tr.OutputChannel
+		tr.Bolts[i].Update(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte(out.bucket))
+			return b.Put([]byte(out.key), out.b)
+		})
 	}
 }
